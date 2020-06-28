c89cf8a068f5aea3f2a08d993fdb1b1f90861211
codelikeamexican <codelikeamexican@gmail.com>
2020-06-28 13:23:25 -0700

    Clases, Módulos y Generadores

--- src/es6/index.js
+++ src/es6/index.js
@@ -118,3 +118,43 @@ const helloPromice = () => {
 helloPromice()
 .then(response =>console.log(response))
 .catch(error=> console.log(error));
+
+
+//** Clases modulos y generadores */
+
+class Calculator {
+    constructor(){
+        this.valueA = 0;
+        this.valueB = 0;
+    }
+
+    sum(valueA, valueB){
+        this.valueA = valueA;
+        this.valueB = valueB;
+        return this.valueB+this.valueA;
+    }
+}
+
+const calc = new Calculator();
+console.log(calc.sum(2,3));
+
+import{ hello } from './module';
+
+hello();
+
+function* helloWorld(){
+    if(true){
+        yield 'Hello, ';
+    }
+    if(true){
+        yield 'World';
+    }
+}
+
+const generator = helloWorld();
+
+console.log(generator.next().value);
+
+console.log(generator.next().value);
+
+
