d2ee3fbd8c495d7c9cfa0357e85512e81c46d021
codelikeamexican <codelikeamexican@gmail.com>
2020-07-01 18:53:05 -0700

    implementacion es8

---
+++ src/es8/index.js
@@ -0,0 +1,21 @@
+const data = {
+    frontend: 'Oscar',
+    backend : 'isabel',
+    design: 'Ana',
+}
+const values = Object.values(data);
+console.log(values.length);
+
+const entries = Object.entries(data);
+console.log(entries);
+
+console.log(entries.length);
+
+const string = 'hola';
+console.log(string.padStart(7,'hi'));
+console.log(string.padEnd(12,'---'));
+
+
+
+
+
