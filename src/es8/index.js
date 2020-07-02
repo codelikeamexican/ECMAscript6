9f9a88d8007765372e5fc0fb36a38d7ee04f4d69
codelikeamexican <codelikeamexican@gmail.com>
2020-07-01 19:01:50 -0700

    functions :)

--- src/es8/index.js
+++ src/es8/index.js
@@ -17,5 +17,29 @@ console.log(string.padEnd(12,'---'));
 
 
 
+//Async Await
 
+const helloWorld = () => {
+    return new Promise((resolve,reject) =>{
+        (false)
+        ? setTimeout( () => resolve('Hello World'), 3000)
+        : reject(new Error('Test Error'))
+    })
+};
 
+const helloAsync = async() =>{
+    const hello = await helloWorld();
+    console.log(hello);
+}
+
+const anotherFunction = async () =>
+{
+    try {
+        const hello = await helloWorld();
+        console.log(hello);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+anotherFunction();
